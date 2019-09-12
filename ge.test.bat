7e9e69e5abfae5d3b0639df85c59faf29322619e
Steve-Cheung-emct <dongfang0571@outlook.com>
2019-09-12 18:43:56 +0800

    Add files via upload
    修復 便簽的对齐錯誤；修復了頁碼格式，調整了欄綫。

--- ge.test.bat
+++ ge.test.bat
@@ -1,2 +1,17 @@
-ptex2pdf -l -u  test
-pause
+uplatex	test
+ptex2pdf -l -u test
+del *.aux /s
+del *.bak /s
+del *.log /s
+del *.bbl /s
+del *.dvi /s
+del *.blg /s
+del *.thm /s
+#del *.toc /s
+del *.out /s
+del *.lof /s
+del *.lol /s
+del *.lot /s
+del *.fdb_latexmk /s
+del *.synctex.gz /s
+exit
